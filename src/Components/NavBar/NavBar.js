61fe8ab2bf4da7f8ddb550af56cca660236f4362
Anit Kumar <anikumarsingh99@gmail.com>
2019-05-02 13:13:00 +0530

    404 not found issues

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,10 +7,10 @@ const Nav = () =>{
         <div className="nav-container">
             <nav className="nav">
                 <ul className="nav-list">
-                    <li><a href="/" className="links-place">Keyword</a></li>
-                    <li><a href="/" className="links-place">Extended</a></li>
-                    <li><a href="/" className="links-place">ASIN</a></li>
-                    <li><a href="/" className="links-place">Translate</a></li>
+                    <li><a href="/Research-Keyword-Tool/Keyword" className="links-place">Keyword</a></li>
+                    <li><a href="/Research-Keyword-Tool/Extended" className="links-place">Extended</a></li>
+                    <li><a href="/Research-Keyword-Tool/ASIN" className="links-place">ASIN</a></li>
+                    <li><a href="/Research-Keyword-Tool/Translate" className="links-place">Translate</a></li>
                 </ul>
             </nav>
       </div>
